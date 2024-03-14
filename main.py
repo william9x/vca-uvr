dccf7dd77711965bd32ac5898121a7046da4868f
LiamH <lehoangnam197@gmail.com>
2024-03-14 16:16:14 +0700

    update mdx config

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 separator = Separator(
     model_file_dir="models",
     output_dir="output",
-    mdx_params={"hop_length": 1024, "segment_size": 256, "overlap": 0.5, "batch_size": 4, "enable_denoise": True},
+    mdx_params={"hop_length": 1024, "segment_size": 256, "overlap": 0.5, "batch_size": 16, "enable_denoise": True},
 )
 separator.load_model("Kim_Vocal_2.onnx")
 app = FastAPI()
