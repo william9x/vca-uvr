b6682e637d7581de18480ca3c3bf2b05d78507cb
LiamH <lehoangnam197@gmail.com>
2024-03-14 16:05:03 +0700

    feat: Download youtube

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 from audio_separator.separator import Separator
 from fastapi import FastAPI
+from moviepy.editor import *
 from pydantic import BaseModel
+from pytube import YouTube
 from starlette.responses import JSONResponse
 
 logger = logging.getLogger(__name__)
@@ -18,6 +20,9 @@ app = FastAPI()
 
 
 class UvrInferReq(BaseModel):
+    task_id: str
+    youtube_path: str
+    output_path: str
     input_path: Path
 
 
@@ -25,6 +30,22 @@ class UvrInferReq(BaseModel):
 async def uvr_infer(req: UvrInferReq) -> JSONResponse:
     print(f"received request: {req}")
 
-    separator.separate(req.input_path)
+    if req.youtube_path is None:
+        separator.separate(req.input_path)
+
+    yt = YouTube(req.youtube_path)
+    yt = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').last()
+    if yt is None:
+        return JSONResponse(content={"message": "No downloadable mp4 from provided URL"}, status_code=400)
+
+    downloaded_file = yt.download(output_path=req.output_path)
+    VideoFileClip(downloaded_file).audio.write_audiofile("downloads/out.mp3")
+    separator.separate("downloads/out.mp3")
 
     return JSONResponse(content={"message": "Created"}, status_code=201)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8082)
