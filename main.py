f858b3440f3b6c5c31fb28d7beca1486f72bca03
LiamH <lehoangnam197@gmail.com>
2024-03-15 18:36:38 +0700

    refactor

--- main.py
+++ main.py
@@ -8,24 +8,26 @@ from pydantic import BaseModel
 from pytube import YouTube
 from starlette.responses import JSONResponse
 
-DOWNLOAD_PATH = os.getenv("UVR_DOWNLOAD_PATH")
-if DOWNLOAD_PATH is None:
-    DOWNLOAD_PATH = "audio/uvr_downloads"
 
-PROCESSED_PATH = os.getenv("UVR_PROCESSED_PATH")
-if PROCESSED_PATH is None:
-    PROCESSED_PATH = "audio/uvr_processed"
+def get_env(key, default_val=None):
+    val = os.getenv(key)
+    if val is None:
+        return default_val
+    return val
 
-MODEL_PATH = os.getenv("UVR_MODEL_PATH")
-if MODEL_PATH is None:
-    MODEL_PATH = "Kim_Vocal_2.onnx"
+
+DOWNLOAD_PATH = get_env("UVR_DOWNLOAD_PATH", "audio/uvr_downloads")
+PROCESSED_PATH = get_env("UVR_PROCESSED_PATH", "audio/uvr_processed")
+MODEL_PATH = get_env("UVR_MODEL_PATH", "Kim_Vocal_2.onnx")
+VIDEO_EXT = get_env("UVR_VIDEO_EXT", "mp4")
+AUDIO_EXT = get_env("UVR_VIDEO_EXT", "mp3")
 
 logger = logging.getLogger(__name__)
 
 separator = Separator(
     model_file_dir="models",
     output_dir=PROCESSED_PATH,
-    output_format="mp3",
+    output_format=AUDIO_EXT,
     mdx_params={"hop_length": 1024, "segment_size": 256, "overlap": 0.25, "batch_size": 24, "enable_denoise": True},
 )
 separator.load_model(MODEL_PATH)
@@ -47,16 +49,20 @@ async def uvr_infer(req: UvrInferReq) -> JSONResponse:
             separator.separate(req.input_path)
 
         yt = YouTube(req.youtube_path)
-        yt = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').last()
+        yt = yt.streams.filter(progressive=True, file_extension=VIDEO_EXT).order_by('resolution').last()
         if yt is None:
-            return JSONResponse(content={"message": "No downloadable mp4 from provided URL"}, status_code=400)
+            return JSONResponse(content={"message": f"No downloadable {VIDEO_EXT} from provided URL"}, status_code=400)
 
         file_prefix = req.task_id.replace("-", "_")
-        downloaded_file = yt.download(output_path=DOWNLOAD_PATH, filename=f"{file_prefix}.mp4")
-        VideoFileClip(downloaded_file).audio.write_audiofile(f"{DOWNLOAD_PATH}/{file_prefix}.mp3")
 
-        process_files = separator.separate(f"{DOWNLOAD_PATH}/{file_prefix}.mp3")
-        return JSONResponse(content={"message": "Created", "file": f"{process_files[0]}"}, status_code=201)
+        downloaded_file = yt.download(output_path=DOWNLOAD_PATH, filename=f"{file_prefix}.{VIDEO_EXT}")
+
+        audio_file_path = f"{DOWNLOAD_PATH}/{file_prefix}.{AUDIO_EXT}"
+        VideoFileClip(downloaded_file).audio.write_audiofile(audio_file_path)
+
+        processed_files = separator.separate(audio_file_path)
+
+        return JSONResponse(content={"message": "Created", "file": f"{processed_files[0]}"}, status_code=201)
     except Exception as e:
         return JSONResponse(content={"message": f"UVR error {e}"}, status_code=500)
 
