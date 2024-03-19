c936ab53c2d0f0b584019926d43cd60c3d656e05
LiamH <lehoangnam197@gmail.com>
2024-03-19 16:36:25 +0700

    fix convert mp4 to mp3

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ async def uvr_infer(req: UvrInferReq) -> JSONResponse:
         audio_file_path = req.input_path
         if audio_file_path.suffix == VIDEO_EXT:
             audio_file_path = audio_file_path.with_suffix(AUDIO_EXT)
-            VideoFileClip(req.input_path).audio.write_audiofile(audio_file_path)
+            VideoFileClip(req.input_path.absolute()).audio.write_audiofile(audio_file_path.absolute())
 
         processed_files = separator.separate(audio_file_path)
         if len(processed_files) != 2:
